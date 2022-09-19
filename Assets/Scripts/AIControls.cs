006c6f3ea30b4150a1fa779fb460412ef4e75a97
hamburgerghini <tommipontinen76@gmail.com>
2022-09-19 15:31:51 +0300

    Created AI

--- Assets/Scripts/AIControls.cs
+++ Assets/Scripts/AIControls.cs
@@ -8,20 +8,40 @@ public class AIControls : MonoBehaviour
     public float turningSpeed;
     public float turretTurningSpeed;
     public float shootingCooldown;
+    public float detectRange;
+    public float stoppingRange;
+    public float switchTargetDistance;
+    public float switchDistance;
+
+    public float AIDelay;
 
     public Transform turret;
     public Transform muzzle;
     public GameObject projectile;
 
+    public string stringState;
+    
     private Rigidbody rb;
     private float t;
+    private float AIt;
 
     private GameObject targetObject;
     private Vector3 target;
 
+    private int obstacleMask;
+
+    private enum State { forward, left, right, back, stop };
+    private State state;
+    private State nextState;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        t = 0f;
+        AIt = 0f;
+        obstacleMask = LayerMask.GetMask("Obstacle");
+        state = State.forward;
+        nextState = State.forward;
     }
 
     void FixedUpdate()
@@ -29,6 +49,8 @@ public class AIControls : MonoBehaviour
         Vector3 currentRotation = rb.rotation.eulerAngles;
         rb.rotation = Quaternion.Euler(0f, currentRotation.y, 0f);
         
+
+        // Etsitään pelaajia
         if(targetObject != null)
         {
             target = targetObject.transform.position;
@@ -39,20 +61,40 @@ public class AIControls : MonoBehaviour
         }
 
         float angle = Vector3.SignedAngle(transform.forward, target - transform.position, Vector3.up);
-
-        if(angle < 0)
+        
+        // Tilakone
+        if(state ==State.forward)
         {
-            Turning(-1f);
+            stringState = "forward";
+            if(angle < 0)
+            {
+                Turning(-1f);
+            }
+            else if (angle > 0)
+            {
+                Turning(1f);
+            }
+            if(Mathf.Abs(angle) < 90)
+            {
+                Move(1f);
+            }
         }
-        else if (angle > 0)
+        else if(state == State.left) 
         {
-            Turning(1f);
+           stringState = "left"; 
         }
-        if(Mathf.Abs(angle) < 90)
+        else if(state == State.right) 
         {
-            Move(1f);
+            stringState = "right";
         }
-
+        else if(state == State.back) 
+        {
+            stringState = "back";
+        }
+        else if(state == State.stop) 
+        {
+            stringState = "stop";
+        }
     }
 
     private void Move(float input)
